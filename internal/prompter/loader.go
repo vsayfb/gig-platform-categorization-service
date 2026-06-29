059e48b280a061ed05c4fc6d66c9668b184848f2
vsay <96926401+vsayfb@users.noreply.github.com>
2026-06-29 15:25:59 +0200

    fix(embed): use go embed to store AI system prompt

--- internal/prompter/loader.go
+++ internal/prompter/loader.go
@@ -6,25 +6,18 @@ import (
 )
 
 //go:embed prompts/profession.txt
-var embeddedProfessionPrompt string
-
 var professionPrompt string
 
 func Init(promptFile string) error {
-
-	professionPrompt = embeddedProfessionPrompt
-
 	if promptFile == "" {
 		return nil
 	}
 
 	data, err := os.ReadFile(promptFile)
-
 	if err != nil {
 		return err
 	}
 
 	professionPrompt = string(data)
-
 	return nil
 }
